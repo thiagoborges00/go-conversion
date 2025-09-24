5dddc7e01ac8467c64ef3c67c6e4e7f4bbd71933
Thiago Borges <thandbor@gmail.com>
2025-09-24 17:25:30 -0300

    feat: add comand for

---
+++ for/forr.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main(){
+	fmt.Println("for")
+	for i := 0; i < 10; i++ {
+		fmt.Println(i)
+	}
+	// for nao tem parentesis
+
+	x:=1
+	for x <=10{
+		fmt.Println("==> ", x)
+		x++
+	}
+
+}
