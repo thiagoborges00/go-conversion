db7454863d152ac75b9f808f26e637343868f9bd
Thiago Borges <thandbor@gmail.com>
2025-09-29 17:47:54 -0300

    feat: add if

---
+++ if/main.go
@@ -0,0 +1,16 @@
+package main
+ import "fmt"
+
+ func main(){
+	x:="flamengo"
+	if (x=="santos"){
+		fmt.Println("santos")
+	}
+	elseif (x == "flamengo"){
+		fmt.Println("Flamengo")
+		
+	}
+	else{
+		fmt.Println("Corinthians")
+	}
+ }
